2cd0b4e062c44e0d8537ca45b06d73dd2ece71ef
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-04-24 00:17:04 +0100

    fixed return error

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,9 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  Promise.allSettled([
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
+    uploadPhoto(filename),
   ]);
 }
