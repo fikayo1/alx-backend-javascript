0f0fa7d1b290c9d572095f364408cca9f7cfcbf3
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-05-01 22:22:36 +0100

    Fixed error

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,18 +1,22 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const newStu = students.filter((student) => (student.location === city));
+  if (!Array.isArray(students)) {
+    return [];
+  }
 
-  newStu.map((student) => {
-    let grade = newGrades.filter((grade) => (student.id === grade.studentId));
-    if (grade.length === 0) {
-      grade = 'N/A';
-    } else {
-      grade = grade[0].grade;
-    }
-    return {
-      id: student.id,
-      firstName: student.firstName,
-      location: student.location,
-      grade,
-    };
-  });
+  return students
+    .filter((student) => (student.location === city))
+    .map((student) => {
+      let grade = newGrades.filter((grade) => (student.id === grade.studentId));
+      if (grade.length === 0) {
+        grade = 'N/A';
+      } else {
+        grade = grade[0].grade;
+      }
+      return {
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade,
+      };
+    });
 }
