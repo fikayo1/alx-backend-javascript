08253ad262726ee8637fed47904c1e564a0da0f2
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-07-11 22:34:49 +0100

    updated all

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,14 +3,14 @@ const calculateNumber = require('./1-calcul.js');
 
 describe("calculateNumber", function() {
     it("should round the numbers and return their sum", function() {
-        assert.equal(calculateNumber(1.2, 3.7, "SUM"), 5);
+        assert.equal(calculateNumber("SUM",1.2, 3.7), 5);
     });
     it("should round the numbers and return their difference", function() {
-        assert.equal(calculateNumber(7.2, 3.2, "SUBTRACT"), 4);
+        assert.equal(calculateNumber("SUBTRACT", 7.2, 3.2), 4);
     });
     it("should round the numbers and return their Division", function() {
-        assert.equal(calculateNumber(6.2, 3.3, "DIVIDE"), 2);
-        assert.equal(calculateNumber(6.2, 0.1, "DIVIDE"), "Error");
+        assert.equal(calculateNumber("DIVIDE", 6.2, 3.3, ), 2);
+        assert.equal(calculateNumber("DIVIDE", 6.2, 0.1), "Error");
 
     });
 });
