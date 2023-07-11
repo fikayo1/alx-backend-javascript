fba50e3f989f022b6f623bc8a90d9d4406923959
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-07-12 00:40:02 +0100

    edited module

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -4,3 +4,4 @@ function sendPaymentRequestToApi(totalAmount, totalShipping) {
     const sum = Utils.calculateNumber("SUM", totalAmount, totalShipping);
     console.log(`The total is: ${sum}`);
 }
+module.exports = sendPaymentRequestToApi
