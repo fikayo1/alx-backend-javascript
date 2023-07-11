330fca9c6aa6fdc64744205bf08a823930649808
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-07-11 22:44:48 +0100

    corrected error

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,17 +1,17 @@
 const chai = require("chai");
-const except = chai.except;
+const expect = chai.expect;
 const calculateNumber = require('./2-calcul_chai.js');
 
 describe("calculateNumber", function() {
     it("should round the numbers and return their sum", function() {
-        except(calculateNumber("SUM",1.2, 3.7)).to.equal(5);
+        expect(calculateNumber("SUM",1.2, 3.7)).to.equal(5);
     });
     it("should round the numbers and return their difference", function() {
-        except(calculateNumber("SUBTRACT", 7.2, 3.2)).to.equal(4);
+        expect(calculateNumber("SUBTRACT", 7.2, 3.2)).to.equal(4);
     });
     it("should round the numbers and return their Division", function() {
-        except(calculateNumber("DIVIDE", 6.2, 3.3, )).to.equal(2);
-        except(calculateNumber("DIVIDE", 6.2, 0.1)).to.equal("Error");
+        expect(calculateNumber("DIVIDE", 6.2, 3.3, )).to.equal(2);
+        expect(calculateNumber("DIVIDE", 6.2, 0.1)).to.equal("Error");
 
     });
 });
